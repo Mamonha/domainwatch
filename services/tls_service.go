8dfb4424e5703bd75a08905038ad366e4711576d
Mamonha <mamonha302@gmail.com>
2025-10-19 15:58:04 -0300

    fix: refactor CheckTLSVersion function to simplify command execution logic

--- services/tls_service.go
+++ services/tls_service.go
@@ -8,25 +8,33 @@ import (
 func CheckTLSVersion(domain string) (map[string]bool, error) {
 	result := make(map[string]bool)
 
-	var cmdBuilder func(string) *exec.Cmd
-	if strings.Contains(domain, "/") || strings.Count(domain, ".") > 1 {
-		cmdBuilder = func(tlsVersion string) *exec.Cmd {
-			return exec.Command("curl", "-k", tlsVersion, "https://"+domain)
+	if strings.Contains(domain, "/") {
+		for _, tlsVersion := range []string{"--tlsv1.0", "--tlsv1.1", "--tlsv1.2", "--tlsv1.3"} {
+			cmd := exec.Command("curl", "-k", tlsVersion, "https://"+domain)
+			if err := cmd.Run(); err == nil {
+				result[tlsVersion] = true
+			} else {
+				result[tlsVersion] = false
+			}
+		}
+	} else if strings.Count(domain, ".") == 1 {
+		for _, tlsVersion := range []string{"tls1", "tls1_1", "tls1_2", "tls1_3"} {
+			cmd := exec.Command("openssl", "s_client", "-connect", domain+":443", "-"+tlsVersion)
+			if err := cmd.Run(); err == nil {
+				result[tlsVersion] = true
+			} else {
+				result[tlsVersion] = false
+			}
 		}
 	} else {
-		cmdBuilder = func(tlsVersion string) *exec.Cmd {
-			return exec.Command("openssl", "s_client", "-connect", domain+":443", "-"+tlsVersion)
+		for _, tlsVersion := range []string{"--tlsv1.0", "--tlsv1.1", "--tlsv1.2", "--tlsv1.3"} {
+			cmd := exec.Command("curl", "-k", tlsVersion, "https://"+domain)
+			if err := cmd.Run(); err == nil {
+				result[tlsVersion] = true
+			} else {
+				result[tlsVersion] = false
+			}
 		}
 	}
-
-	for _, tlsVersion := range []string{"tls1", "tls1_1", "tls1_2", "tls1_3", "--tlsv1.0", "--tlsv1.1", "--tlsv1.2", "--tlsv1.3"} {
-		cmd := cmdBuilder(tlsVersion)
-		if err := cmd.Run(); err == nil {
-			result[tlsVersion] = true
-		} else {
-			result[tlsVersion] = false
-		}
-	}
-
 	return result, nil
 }
