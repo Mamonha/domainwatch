512909cd15cf5b90c35ca2ca3d1171d91a438e14
Mamonha <mamonha302@gmail.com>
2025-10-19 15:55:54 -0300

    fix: implement CheckTLSVersion function to check supported TLS versions for a domain

--- services/tls_service.go
+++ services/tls_service.go
@@ -1,6 +1,32 @@
 package services
 
-// func CheckTLSVersion(string domain) bool {
+import (
+	"os/exec"
+	"strings"
+)
 
-// 	return true
-// }
+func CheckTLSVersion(domain string) (map[string]bool, error) {
+	result := make(map[string]bool)
+
+	var cmdBuilder func(string) *exec.Cmd
+	if strings.Contains(domain, "/") || strings.Count(domain, ".") > 1 {
+		cmdBuilder = func(tlsVersion string) *exec.Cmd {
+			return exec.Command("curl", "-k", tlsVersion, "https://"+domain)
+		}
+	} else {
+		cmdBuilder = func(tlsVersion string) *exec.Cmd {
+			return exec.Command("openssl", "s_client", "-connect", domain+":443", "-"+tlsVersion)
+		}
+	}
+
+	for _, tlsVersion := range []string{"tls1", "tls1_1", "tls1_2", "tls1_3", "--tlsv1.0", "--tlsv1.1", "--tlsv1.2", "--tlsv1.3"} {
+		cmd := cmdBuilder(tlsVersion)
+		if err := cmd.Run(); err == nil {
+			result[tlsVersion] = true
+		} else {
+			result[tlsVersion] = false
+		}
+	}
+
+	return result, nil
+}
